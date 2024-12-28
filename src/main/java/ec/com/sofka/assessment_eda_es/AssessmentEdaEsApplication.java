16d2f4135a8db45284e4df5287bb5b47065fe24b
Mishell Yagual Mendoza <mishell.yagual@sofka.com.co>
2024-12-28 12:23:33 -0500

    Initial commit

---
+++ src/main/java/ec/com/sofka/assessment_eda_es/AssessmentEdaEsApplication.java
@@ -0,0 +1,13 @@
+package ec.com.sofka.assessment_eda_es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentEdaEsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentEdaEsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/com/sofka/assessment_eda_es/AssessmentEdaEsApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.sofka.assessment_eda_es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentEdaEsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
